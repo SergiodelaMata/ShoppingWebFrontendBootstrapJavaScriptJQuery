1a4e6d2e5d6b258a20d5d9c33eef671bccf355bf
SergiodelaMata <sergio.matam@edu.uah.es>
2022-03-07 20:46:00 +0100

    :sparkles: Changed structure to insert the data from the categories and their products.

--- js/categories.js
+++ js/categories.js
@@ -1,50 +1,38 @@
-function getArrayCategories()
+function getData()
 {
-    return $.getJSON("files/categories.json");
+    return $.getJSON("files/test.json");
 }
 
-function getArrayProducts()
-{
-    return $.getJSON("files/products.json");
-}
+$(".main-container.collapse").on('shown.bs.collapse', function () {    
+        $(".main-container.collapse").not($(this)).collapse('hide');
+});
 
-function getListProducts(){
-    var json = getArrayProducts().done(function(jsonProducts){
-        return jsonProducts;
-    });
-    return json;
-}
+getData().done(function(json){
+    var categories = json.categories;
+    var products = json.products;
+    var users = json.users;
 
-
-getArrayCategories().done(function(jsonCategories) {
-    console.log(jsonCategories);
     var structureCategory = "";
-
-    for(var i = 0; i < jsonCategories.length; i++)
+    for(var i = 0; i < categories.length; i++)
     {
-        structureCategory += "<a href='products" + (i+1) + "' class='col-sm-12 card btn btn-default collapsed justify-content-start text-dark' data-toggle='collapse' style='text-align:left'>" 
-            + "<p style='margin-top: 0em; margin-bottom: 0em;'>" + jsonCategories[i].nameCategory + "</p>"
+        structureCategory += "<a href='#products" + (i+1) + "' class='col-sm-12 card btn btn-default collapsed justify-content-start text-dark' data-toggle='collapse' style='text-align:left'>" 
+            + "<p style='margin-top: 0em; margin-bottom: 0em;'>" + categories[i].nameCategory + "</p>"
             + "</a>" 
-            + "<div id='products" + (i+1) + "' class='row'>";
+            + "<div id='products" + (i+1) + "' class='container main-container collapse in'><div id='listProducts" + (i+1) + "' class='row'>" ;
 
-        console.log(getListProducts());
-        /*getArrayProducts().done(function(jsonProducts){
-            console.log(jsonProducts);
-            var structureProducts = "";
-            for(var j = 0; j < jsonProducts.length; j++)
+        var structureProducts = "";
+        for(var j = 0; j < products.length; j++)
+        {
+            if(products[j].idCategory === categories[i].idCategory)
             {
-                console.log(jsonCategories[i]);
-                if(jsonProducts[j].idCategory === jsonCategories[i].idCategory)
-                {
-                    structureProducts += "<div class='card col-sm-3'>" + jsonProducts[j].titleProduct + "</div>";
-                }
-                
+                structureProducts += "<div class='card col-sm-3'>" + products[j].titleProduct + "</div>";
             }
-            structureCategory += structureProducts;
-        });*/
-        structureCategory = "</div>";
+            
+        }
+        structureCategory += structureProducts;
+        structureCategory += "</div></div>";
     }
     $("#categories").append(structureCategory);
-    
 
 });
+
