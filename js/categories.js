8be54c6fc1c99a3e8c0d79687d4bf9216a7319df
SergiodelaMata <sergio.matam@edu.uah.es>
2022-03-08 21:13:59 +0100

    :sparkles: Update the structure and data shown from products. :sparkles: Update how it is shown the content of the different categories.

--- js/categories.js
+++ js/categories.js
@@ -3,10 +3,6 @@ function getData()
     return $.getJSON("files/test.json");
 }
 
-$(".main-container.collapse").on('shown.bs.collapse', function () {    
-        $(".main-container.collapse").not($(this)).collapse('hide');
-});
-
 getData().done(function(json){
     var categories = json.categories;
     var products = json.products;
@@ -15,22 +11,40 @@ getData().done(function(json){
     var structureCategory = "";
     for(var i = 0; i < categories.length; i++)
     {
-        structureCategory += "<a href='#products" + (i+1) + "' class='col-sm-12 card btn btn-default collapsed justify-content-start text-dark' data-toggle='collapse' style='text-align:left'>" 
-            + "<p style='margin-top: 0em; margin-bottom: 0em;'>" + categories[i].nameCategory + "</p>"
-            + "</a>" 
-            + "<div id='products" + (i+1) + "' class='container main-container collapse in'><div id='listProducts" + (i+1) + "' class='row'>" ;
+        structureCategory += "<div class='accordion-item'>"
+            + "<h5 class='accordion-header' id='category" + (i+1)  + "'>"
+                + "<button type='button' data-bs-toggle='collapse' data-bs-target='#products" + (i+1) + "' class='col-sm-12 accordion-button justify-content-start text-dark' aria-expanded='true' aria-controls='products" + (i+1) + "' style='text-align:left; margin-bottom:0.5em'>" 
+                + categories[i].nameCategory
+                + "</button>" 
+            + "</h5>"
+            + "<div id='products" + (i+1) + "' class='container accordion-collapse collapse show' aria-labelledby='category" + (i+1)  + "'><div id='listProducts" + (i+1) + "' class='row' style='margin-bottom:0.5em;'>" ;
 
         var structureProducts = "";
         for(var j = 0; j < products.length; j++)
         {
             if(products[j].idCategory === categories[i].idCategory)
             {
-                structureProducts += "<div class='card col-sm-3'>" + products[j].titleProduct + "</div>";
+                structureProducts += 
+                    "<div class='card col-sm-6' style='margin-bottom:0.5em;'>" 
+                        + "<div class='row'>"
+                            + "<div class='col-sm-4 align-self-center' style='text-align:center;'>"
+                                + "<img src='" + products[j].image + "' style='width:100%;'/>"
+                                + "<p style='font-size:0.8em;'>Unidades: " + products[j].numUnits + "</p>"
+                                + "<h3 class='text-primary'>" + products[j].price + "€</h3>"
+                                + "<button class='btn btn-primary' type='button' style='width:100%;'>Añadir a<br>la cesta</button>"
+                            + "</div>"
+                            + "<div class='card-body col-sm-8'>"
+                                + "<strong>" + products[j].titleProduct + "</strong>" 
+                                + "<p class='text-muted' style='font-size:0.8em;'>" + products[j].codeProduct + "</p>"
+                                + "<p style='font-size:0.8em;'>" + products[j].description + "</p>"
+                            + "</div>"
+                        + "</div>"
+                    + "</div>";
             }
             
         }
         structureCategory += structureProducts;
-        structureCategory += "</div></div>";
+        structureCategory += "</div></div></div>";
     }
     $("#categories").append(structureCategory);
 
